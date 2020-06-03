c8124fc3a42c9270cd0a1e49985861ce0085bb47
Lavrov Aleksandr <lismut@gmail.com>
2020-06-04 00:32:23 +0300

    io_context to service

--- main.cpp
+++ main.cpp
@@ -75,8 +75,8 @@ private:
 class server
 {
 public:
-    server(boost::asio::io_context& io_context, short port, short _bulk_size)
-            : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)), bulk_size(_bulk_size)
+    server(boost::asio::io_service& io_con, short port, short _bulk_size)
+            : acceptor_(io_con, tcp::endpoint(tcp::v4(), port)), bulk_size(_bulk_size)
     {
         do_accept();
     }
@@ -124,11 +124,11 @@ int main(int argc, char* argv[])
             return 1;
         }
 
-        boost::asio::io_context io_context;
+        boost::asio::io_service io_con;
 
-        server server(io_context, port, bulk_size);
+        server server(io_con, port, bulk_size);
 
-        io_context.run();
+        io_con.run();
     }
     catch (const std::exception& ex)
     {
