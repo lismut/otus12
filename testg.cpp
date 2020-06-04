60dfb94416b8bb3a695da2c6acdebf8e6eedb2e4
Lavrov Aleksandr <lismut@gmail.com>
2020-06-04 19:35:51 +0300

    repaired tests, shared error

--- testg.cpp
+++ testg.cpp
@@ -5,19 +5,19 @@
 
 std::string openS = openStr();
 std::string closeS = closeStr();
+std::shared_ptr<QueueString> qq = std::make_shared<QueueString>();
 
 TEST(GOOGLE_TEST1, test1)
 {
-    std::shared_ptr<QueueString> qq;
     bulkManager man(2);
     man.newString("1", qq);
     man.newString("2", qq);
-    ASSERT_TRUE(man.getBulk().size() == 0);
+    size_t sz = man.getBulk().size();
+    ASSERT_TRUE( sz == 0);
 }
 
 TEST(GOOGLE_TEST2, test2)
 {
-    std::shared_ptr<QueueString> qq;
     bulkManager man(7);
     man.newString("1", qq);
     man.newString("2", qq);
@@ -27,7 +27,6 @@ TEST(GOOGLE_TEST2, test2)
 
 TEST(GOOGLE_TEST3, test3)
 {
-    std::shared_ptr<QueueString> qq;
     bulkManager man(3);
     man.newString("1", qq);
     man.newString("2", qq);
@@ -36,7 +35,6 @@ TEST(GOOGLE_TEST3, test3)
 
 TEST(GOOGLE_TEST4, test4)
 {
-    std::shared_ptr<QueueString> qq;
     bulkManager man(6);
     man.newString(openS, qq);
     man.newString("2", qq);
@@ -50,7 +48,6 @@ TEST(GOOGLE_TEST4, test4)
 
 TEST(GOOGLE_TEST5, test5)
 {
-    std::shared_ptr<QueueString> qq;
     bulkManager man(6);
     man.newString(openS, qq);
     man.newString("2", qq);
@@ -66,7 +63,6 @@ TEST(GOOGLE_TEST5, test5)
 
 TEST(GOOGLE_TEST6, test6)
 {
-    std::shared_ptr<QueueString> qq;
     bulkManager man(6);
     man.newString(openS, qq);
     man.newString("2", qq);
@@ -82,7 +78,6 @@ TEST(GOOGLE_TEST6, test6)
 
 TEST(GOOGLE_TEST7, test7)
 {
-    auto qq = std::make_shared<QueueString>();
     std::size_t bulk = 5;
     auto h = async::connect(bulk);
     auto h2 = async::connect(bulk);
